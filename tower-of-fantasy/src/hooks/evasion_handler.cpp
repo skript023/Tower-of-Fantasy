5a4226389423c640db13ac9a6efe6a6c7d7aee61
skript023 <fatur_muhammad@bk.ru>
2022-09-19 23:04:11 +0700

    Added New Hooks

--- tower-of-fantasy/src/hooks/evasion_handler.cpp
+++ tower-of-fantasy/src/hooks/evasion_handler.cpp
@@ -10,7 +10,10 @@ namespace big
 
 		if (g_settings->player.infinite_dodge)
 		{
-			return (__int64)(signed int)floor((float)(v1 * *(float*)g_pointers->dword_7FF681753804) - 3.0);
+			if (GetAsyncKeyState(VK_LSHIFT) & 0x1)
+			{
+				return (__int64)(signed int)floor((float)(v1 * *(float*)g_pointers->dword_7FF681753804) - 3.0);
+			}
 		}
 
 		return g_hooking->m_evasion_handler_hook.get_original<decltype(&evasion_handler)>()(a1);
