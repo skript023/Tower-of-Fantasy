d2b6e988b81c7f9915300b875e38b0796f4b13e0
skript023 <fatur_muhammad@bk.ru>
2022-09-25 22:11:58 +0700

    Change Name File

---
+++ tower-of-fantasy/src/hooks/game_viewport.cpp
@@ -0,0 +1,25 @@
+#include "hooking.hpp"
+#include "script.hpp"
+#include "class/unreal_engine/ucanvas.hpp"
+
+namespace big
+{
+	void hooks::draw_transition(GameViewport* _this, UCanvas* canvas)
+	{
+		TRY_CLAUSE
+		{
+
+			return g_hooking->m_client_viewport_hook.get_original<decltype(&draw_transition)>(hooks::draw_transition_index)(_this, canvas);
+		} EXCEPT_CLAUSE
+	}
+
+	void hooks::post_render(GameViewport* _this, UCanvas* canvas)
+	{
+		TRY_CLAUSE
+		{
+
+
+			return g_hooking->m_client_viewport_hook.get_original<decltype(&post_render)>(hooks::post_render_index)(_this, canvas);
+		} EXCEPT_CLAUSE
+	}
+}
